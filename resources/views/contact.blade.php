42033c441c325989c68df728b0569fa1f3e91930
yuyunpuspitayani <yuyunphrp@gamil.com>
2024-02-21 14:39:40 +0700

    update homecontroller, home blade oho, dan contactblade php. modif web php and composer lock and las delete env example

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Order your soul. Reduce your wants. - Augustine -->
+    Selamat datang di home
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Well begun is half done. - Aristotle -->
+    Selamat datang di home {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+
+</div>
